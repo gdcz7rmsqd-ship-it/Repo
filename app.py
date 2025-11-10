6d669d68ff61f87be752ba2fb833383cce9aca32
gdcz7rmsqd-ship-it <gdcz7rmsqd@privaterelay.appleid.com>
2025-11-10 13:05:59 -0600

    Update message in index route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', message='Hello from cicdinclass')
+    return render_template('index.html', message='Hello is me')
 
 @app.route('/api')
 def api():
